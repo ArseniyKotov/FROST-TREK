c6d791f7ba7ea5e5aff992ab4df5c140f5131849
Arseniy Kotov <kotov.a.arseniy@gmail.com>
2025-04-30 10:02:21 -0700

    correct the outputs

--- src/main.tsx
+++ src/main.tsx
@@ -1,27 +1,9 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
+import './index.css';
 import { Amplify } from 'aws-amplify';
-
-// Inline Amplify configuration
-const output = {
-  "api": {
-    "GraphQLAPIEndpointOutput": "https://example-endpoint.appsync-api.us-west-2.amazonaws.com/graphql",
-    "GraphQLAPIIdOutput": "abcdefghijklmnopqrstuvwxyz",
-    "GraphQLAPIKeyOutput": "da2-abcdefghijklmnopqrstuvwxyz"
-  },
-  "auth": {
-    "IdentityPoolId": "us-west-2:abcdef-1234-5678-9012-abcdefghijkl",
-    "IdentityPoolName": "firewatch_identity_pool",
-    "UserPoolId": "us-west-2_abcdefghi",
-    "UserPoolArn": "arn:aws:cognito-idp:us-west-2:123456789012:userpool/us-west-2_abcdefghi",
-    "UserPoolName": "firewatch_user_pool",
-    "AppClientId": "abcdefghijklmnopqrstuvwxyz",
-    "AppClientIDWeb": "abcdefghijklmnopqrstuvwxyz"
-  }
-};
-
+import output from '../amplify_outputs.json';
 
 Amplify.configure(output);
 
@@ -29,4 +11,4 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
-)
+);
